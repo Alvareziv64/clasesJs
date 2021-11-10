29f158a34aca26ea3c72bbbe6a9b16958609837d
Ivan Alvarez <alvarez.iv64@gmail.com>
2021-11-10 20:23:08 -0300

    clase 5 y 6

---
+++ clase5/clase5.js
@@ -0,0 +1,21 @@
+
+class consolas{ 
+
+    constructor( marca , modelo , anioLanzamiento , valor ){
+
+        this.marca = marca; 
+        this.modelo = modelo;
+        this.anioLanzamiento = anioLanzamiento;
+        this.valor = valor;
+
+    }
+
+}
+
+let nuevaConsola = new consolas( "Nintendo" , "64" , "1996" , "$5500" );
+
+
+console.log( nuevaConsola ); 
+
+
+
---
+++ clase6/clase6.js
@@ -0,0 +1,37 @@
+
+let consolas = [ 
+
+    { marca : "Nintendo",
+      modelo : "64",
+      aniolanzamiento : "1996",
+      valor : "$5500" 
+    },
+
+    { marca : "Sega",
+      modelo : "Genesis Model 2",
+      aniolanzamiento : "1993",
+      valor : "$8000" 
+    },
+
+    { marca : "Nintendo",
+      modelo : "Game Boy Color",
+      aniolanzamiento : "1998",
+      valor : "$6500"
+    },
+
+    { marca : "Microsoft",
+      modelo : "Xbox classic",
+      aniolanzamiento : "2001",
+      valor : "$12000"
+    }
+
+
+];
+
+console.log(consolas);
+
+console.log("En stock:")
+for ( let stock of consolas ) {
+    console.log(stock.marca + " " + stock.modelo + " por " + stock.valor);
+}
+
