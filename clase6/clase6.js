3ee3157a694382c69b4be38076634cff75f4a1a1
Ivan Alvarez <alvarez.iv64@gmail.com>
2021-11-10 20:26:26 -0300

    clase 6

--- clase6/clase6.js
+++ clase6/clase6.js
@@ -4,25 +4,25 @@ let consolas = [
     { marca : "Nintendo",
       modelo : "64",
       aniolanzamiento : "1996",
-      valor : "$5500" 
+      valor : 5500
     },
 
     { marca : "Sega",
       modelo : "Genesis Model 2",
       aniolanzamiento : "1993",
-      valor : "$8000" 
+      valor : 8000
     },
 
     { marca : "Nintendo",
       modelo : "Game Boy Color",
       aniolanzamiento : "1998",
-      valor : "$6500"
+      valor : 6500
     },
 
     { marca : "Microsoft",
       modelo : "Xbox classic",
       aniolanzamiento : "2001",
-      valor : "$12000"
+      valor : 12000
     }
 
 
@@ -32,6 +32,6 @@ console.log(consolas);
 
 console.log("En stock:")
 for ( let stock of consolas ) {
-    console.log(stock.marca + " " + stock.modelo + " por " + stock.valor);
+    console.log(stock.marca + " " + stock.modelo + " por $" + stock.valor);
 }
 
