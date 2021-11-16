2fe6a6b4cbebaa5b540dfdcfd7d9a5280a6fc756
Ivan Alvarez <alvarez.iv64@gmail.com>
2021-11-15 22:54:30 -0300

    1 entrega final v2.5

--- 1ef/1ef.js
+++ 1ef/1ef.js
@@ -1,29 +1,4 @@
 
-//Inicio de sesion
-
-let usuarioSis = "admin";
-let passwordSis = 123456;
-
-
-for (let i = 0; i < 3; i = i + 1){
-
-      let usuario = prompt("Ingrese su usuario");
-      let password = prompt("Ingrese su password");
-
-      if( usuarioSis == usuario && passwordSis == password ) {
-          console.log ("Bienvenido al sistema");
-          alert("¡Bienvenido!");
-          break;
-      }
-
-      else( 
-
-          alert ( "Nombre de usuario o contraseña incorrecto ")
-           );
-
-}
-
-
 //Stock de consolas
 
 let consolas = [ 
@@ -54,23 +29,57 @@ let consolas = [
 
 ];
 
-// ordenado de mas barato a mas caro
+// stock ordenado de mas barato a mas caro
 
 let ordenPorValor = consolas.sort (( a , b) => {
     return a.valor - b.valor
 })
 
+// lista de stock
 
-console.log("En stock:")
-for ( let stock of consolas ) {
-    console.log(stock.marca + " " + stock.modelo + " por $" + stock.valor);
-};
-
-
-let lista = "Consolas en stock \n"
+let lista = "Consolas en stock: \n"
 ordenPorValor.forEach ((consola) => {
     lista += consola.marca + " " + consola.modelo + " por $" + consola.valor + "\n"
 });
 
-alert(lista);
+
+
+// Usuario y contraseña 
+
+let usuarioSis = "admin";
+let passwordSis = 123456;
+
+
+//ciclo
+
+for (let i = 0; i < 3; i = i + 1){
+
+      let usuario = prompt("Ingrese su usuario");
+      let password = prompt("Ingrese su password");
+
+      if( usuarioSis == usuario && passwordSis == password ) {
+
+          console.log ("Bienvenido al sistema");
+          alert("¡Bienvenido!");
+
+          console.log("En stock:")
+          for ( let stock of consolas ) {
+             console.log(stock.marca + " " + stock.modelo + " por $" + stock.valor);
+          };
+
+          alert(lista);
+
+          break;
+      }
+
+      else( 
+
+          alert ( "Nombre de usuario o contraseña incorrecto ")
+           );
+
+}
+
+
+
+
 
